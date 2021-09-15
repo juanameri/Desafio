8a1b5ae7dba1f8f00ead01c7e8a9d07d637aa0f8
juanameri <86881206+juanameri@users.noreply.github.com>
2021-09-14 21:10:07 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+let nombre = prompt("¿Cual es tu nombre?");
+let apellido = prompt("¿Y tu apellido?");
+let saludoBienvenida = "Hola"+" "+ nombre + " " + apellido + ". ¡Bienvenid@ a mi sitio web!"
+
+alert(saludoBienvenida)
