cf13166fb60ce159af30bc5df5c697f8604ed1ba
juanameri <86881206+juanameri@users.noreply.github.com>
2021-09-22 18:48:46 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,18 +6,30 @@ let saludoBienvenida = "Hola"+" "+ nombre + " " + apellido + ". ¡Bienvenid@ a m
 alert("Hola "+ nombre + " " + apellido + ". ¡Bienvenid@ a mi sitio web!")
 */
 
+/*Desafio Clase 2
 const nombreDinamico = prompt("¿Cual es tu nombre?");
 console.log("¡Hola "+nombreDinamico+"! Bienvenid@ al Restaurante");
 
 const cantidadPersonas = Number(prompt("¿Cuantas personas van a ser?"));
 const fumadores = prompt("¿Alguien fuma?");
 
-if ((cantidadPersonas < 5) && (fumadores == "si")){
-    console.log("Ok " + nombreDinamico + ", subí por la escalera de mano derecha y seras ubicado en la terraza");
-} else if ((cantidadPersonas >= 5) && (fumadores == "si")){
-    console.log("Ok " + nombreDinamico + ", subí por la escalera de mano izquierda y seras ubicado en la terraza");
-} else if ((cantidadPersonas < 5) && (fumadores == "no")){
-    console.log("Ok " + nombreDinamico + ", avanza hacia mano derecha y seras ubicado por un asistente");
-} else if ((cantidadPersonas >= 5) && (fumadores == "no")){
-    console.log("Ok " + nombreDinamico + ", avanza hacia mano izquierda y seras ubicado por un asistente");
+if (fumadores == "si"){
+    if (cantidadPersonas >= 5){
+        console.log("Ok " + nombreDinamico + ", subí por la escalera de mano derecha y seras ubicado en la terraza");
+    } else {
+        console.log("Ok " + nombreDinamico + ", subí por la escalera de mano izquierda y seras ubicado en la terraza");
+    }
+} else if (fumadores == "no"){
+    if (cantidadPersonas < 5){
+        console.log("Ok " + nombreDinamico + ", avanza hacia mano derecha y seras ubicado por un asistente");
+    } else {
+        console.log("Ok " + nombreDinamico + ", avanza hacia mano izquierda y seras ubicado por un asistente");
+    }
+}
+*/
+
+for (mesasOcupadasTerraza = 0; mesasOcupadasTerraza <=14; mesasOcupadasTerraza ++){
+    const nombreDinamico = prompt("¿Cual es tu nombre?");
+    console.log("¡Hola "+ nombreDinamico+"! Bienvenid@ al Restaurante");
+    console.log("Ok " + nombreDinamico + ", subí por la escalera y seras ubicado en la terraza");    
 }
