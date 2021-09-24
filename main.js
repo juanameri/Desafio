c242933de63d27d614c35044a0f0849d123d8ee5
juanameri <86881206+juanameri@users.noreply.github.com>
2021-09-24 16:58:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-/*Desafio Clase 1
+/*
+
+//Desafio Clase 1
 let nombre = prompt("¿Cual es tu nombre?");
 let apellido = prompt("¿Y tu apellido?");
 let saludoBienvenida = "Hola"+" "+ nombre + " " + apellido + ". ¡Bienvenid@ a mi sitio web!"
 
 alert("Hola "+ nombre + " " + apellido + ". ¡Bienvenid@ a mi sitio web!")
-*/
 
-/*Desafio Clase 2
+//Desafio Clase 2
 const nombreDinamico = prompt("¿Cual es tu nombre?");
 console.log("¡Hola "+nombreDinamico+"! Bienvenid@ al Restaurante");
 
@@ -26,10 +27,34 @@ if (fumadores == "si"){
         console.log("Ok " + nombreDinamico + ", avanza hacia mano izquierda y seras ubicado por un asistente");
     }
 }
-*/
+
+//Desafio Clase 3
+
+let mesasOcupadasTerraza = 0;
 
 for (mesasOcupadasTerraza = 0; mesasOcupadasTerraza <=14; mesasOcupadasTerraza ++){
     const nombreDinamico = prompt("¿Cual es tu nombre?");
     console.log("¡Hola "+ nombreDinamico+"! Bienvenid@ al Restaurante");
     console.log("Ok " + nombreDinamico + ", subí por la escalera y seras ubicado en la terraza");    
 }
+
+*/
+
+let picada = 50;
+let sopaDeCalabaza = 35;
+let vitelTone = 60;
+let milanesaConPure = 150;
+let tallarinesConTuco = 100;
+let ensaladaCesar = 75;
+let flan = 40;
+let helado = 35;
+let tarta = 30;
+
+function precioTotal (entrada, platoPrincipal, postre){
+    sumaTotal = entrada + platoPrincipal + postre;
+    return console.log("El precio total de su pedido es $"+ sumaTotal);
+}
+
+precioTotal (picada, milanesaConPure, helado);
+precioTotal (vitelTone, ensaladaCesar, flan);
+precioTotal (sopaDeCalabaza, tallarinesConTuco, tarta);
