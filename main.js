da107ed5765c4dc8c0a306dd8a69a642286581dd
juanameri <86881206+juanameri@users.noreply.github.com>
2021-09-29 19:33:57 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ for (mesasOcupadasTerraza = 0; mesasOcupadasTerraza <=14; mesasOcupadasTerraza +
     console.log("Ok " + nombreDinamico + ", subí por la escalera y seras ubicado en la terraza");    
 }
 
-*/
+//Desafio Clase 4
 
 let picada = 50;
 let sopaDeCalabaza = 35;
@@ -58,3 +58,95 @@ function precioTotal (entrada, platoPrincipal, postre){
 precioTotal (picada, milanesaConPure, helado);
 precioTotal (vitelTone, ensaladaCesar, flan);
 precioTotal (sopaDeCalabaza, tallarinesConTuco, tarta);
+
+//Desafio Complementario Clase 4
+
+var precioProducto = 0;
+var precioSocio = 0;
+
+function PrecioProducto (precio){ 
+    console.log('El precio al publico del producto seleccionado es $'+precio);
+    precioProducto = precio;
+    return precioProducto;
+}
+
+function aplicarDescuento (descuento){
+    console.log('Por ser socio se aplica en tus compras un descuento de $'+descuento);
+    precioSocio = precioProducto - descuento;
+    return precioSocio;
+}
+
+function precioConDescuento (){
+    return console.log("Por ser socio, el precio final a pagar sera $"+precioSocio);
+}
+
+PrecioProducto(1000);
+aplicarDescuento (250);
+precioConDescuento ();
+
+PrecioProducto(150);
+aplicarDescuento (25);
+precioConDescuento ();
+
+PrecioProducto(5000);
+aplicarDescuento (500);
+precioConDescuento ();
+
+//Desafio Clase 5
+
+class Comida {
+    constructor(nombre, precio){
+        this.nombre = nombre.toUpperCase();
+        this.precio = Number(precio);
+        this.ordenado = false;
+    }
+    descuentoPrimavera(){
+        this.precio = (this.precio*75)/100;
+    }
+    ordenar(){
+        this.ordenado = true;
+    }
+}
+
+const comida1 = new Comida ("tallarines con tuco", 100);
+const comida2 = new Comida ("ensalada cesar", 75);
+const comida3 = new Comida ("milanesa con pure", 150);
+
+comida1.descuentoPrimavera();
+comida2.descuentoPrimavera();
+comida1.ordenar();
+comida3.ordenar();
+
+*/
+
+//Desafio Clase 6
+
+class Comida {
+    constructor(nombre, precio){
+        this.nombre = nombre.toUpperCase();
+        this.precio = Number(precio);
+        this.ordenado = false;
+    }
+    descuentoPrimavera(){
+        this.precio = (this.precio*75)/100;
+    }
+    ordenar(){
+        this.ordenado = true;
+    }
+}
+
+const comidas = [];
+comidas.push(new Comida("picada", 50));
+comidas.push(new Comida("milanesaConPure", 150));
+comidas.push(new Comida("flan", 40));
+comidas.push(new Comida("helado", 35));
+comidas.push(new Comida("vitelTone", 60));
+comidas.push(new Comida("tallarinesConTuco", 100));
+
+console.log(comidas)
+
+const preciosConDescuentoPrimavera = comidas.map(comida => comida.precio = comida.precio*75/100);
+for (const comida of comidas) {
+    console.log(comida.nombre);
+    console.log(comida.precio);
+}
